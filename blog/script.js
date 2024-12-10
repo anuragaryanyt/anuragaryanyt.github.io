8365cddf100728615e25b9c9f32649263bbeaef4
anuragaryanyt <anuragraj718@gmail.com>
2024-12-10 10:27:19 +0530

    Source Code privacy

--- blog/script.js
+++ blog/script.js
@@ -61,3 +61,19 @@ const scrollUp = () => {
 }
 
 document.addEventListener('scroll', scrollUp)
+
+    // prevent shortcut keys 
+    document.addEventListener('keydown', function (e) {
+		if (e.ctrlKey && (e.key === 'u' || e.key === 'U')) {
+			e.preventDefault();
+		}
+		if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+			e.preventDefault();
+		}
+	});
+	
+	// prevent right clicks
+	document.addEventListener('contextmenu', function (e) {
+		e.preventDefault();
+	});
+	
