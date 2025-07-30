b89c6c829f1e2f1844ccc6bc0f8073c8eeaa3e53
anuragaryanyt <anuragraj718@gmail.com>
2025-07-30 10:20:11 +0530

    .

--- generate_file_index.py
+++ generate_file_index.py
@@ -141,7 +141,7 @@ HTML_TEMPLATE = """<!DOCTYPE html>
       if (
         e.key === "F12" ||
         (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key)) ||
-        (e.ctrlKey && e.key === 'U')
+        (e.ctrlKey && e.key.toLowerCase() === 'u')
       ) {{
         e.preventDefault();
       }}
