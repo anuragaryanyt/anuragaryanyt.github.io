b230237060c009d56dcf493dc000fb5a40cd0889
anuragaryanyt <anuragraj718@gmail.com>
2025-08-30 13:12:46 +0530

    refactored data fetching

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -33,38 +33,3 @@ exports.handler = async function (event, context) {
 };
 
 
-
-
-// const { MongoClient } = require("mongodb");
-
-// let client = null;
-
-// async function getClient() {
-//   if (!client) {
-//     client = new MongoClient(process.env.MONGODB_URI);
-//     await client.connect();
-//   }
-//   return client;
-// }
-
-// exports.handler = async function(event, context) {
-//   try {
-//     const client = await getClient();
-//     const db = client.db("code");
-//     const collection = db.collection("anurag718");
-
-//     const result = await collection.find({}).toArray();
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(data),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: error.message }),
-//     };
-//   }
-// };
-
-
